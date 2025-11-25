79983ba0a7cc9e9b54dee729e47e3401403ca17c
Yannick Steinbach <yas6108@thi.de>
2025-11-25 11:48:09 +0100

    Final commit for submit

--- Creditcard/train.py
+++ Creditcard/train.py
@@ -2,12 +2,10 @@ import copy
 import time
 from pathlib import Path
 import itertools
-import logging
 
 import torch
 import torch.nn as nn
 from sklearn.preprocessing import StandardScaler
-from sympy.plotting.experimental_lambdify import experimental_lambdify
 from torch.utils.data import DataLoader, TensorDataset
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score, confusion_matrix, accuracy_score, precision_score, recall_score
@@ -19,50 +17,49 @@ import mlflow
 from model import MLP
 from focal_loss import FocalLoss
 
-# --------------------------
-# Logging setup
-# --------------------------
-logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO)
-
-# --------------------------
 # Load configuration
-# --------------------------
 config_path = Path(__file__).parent / "experiment_config.yaml"
 with open(config_path, 'r') as f:
     config = yaml.safe_load(f)
 
-# --------------------------
 # Load dataset
-# --------------------------
 csv_path = Path(__file__).parent / "creditcard.csv"
 df = pd.read_csv(csv_path)
 
+# Get full Samples and Class
 X_full = df.drop(columns=["Class"]).values.astype(np.float32)
 y_full = df["Class"].values.astype(np.float32)  # BCE expects float
 
-mlflow.set_tracking_uri("http://localhost:" + "32770")
-mlflow.set_experiment("CF_r1.0_0.5_0.1_e50_b1024_v3")
+mlflow.set_tracking_uri("http://localhost:" + "32771")
+mlflow.set_experiment("Creditcard Fraud")
 
-# --------------------------
-# Function to create stratified loaders
-# --------------------------
 def create_loaders(X, y, batch_size, test_size=0.2, val_size=0.1, random_seed=42):
+    """
+    Creates dataloaders for training, validation and test data splits.
+    :return: Set of torch.Dataloaders
+    """
+    # Split whole data in validation and training data
     X_trainval, X_test, y_trainval, y_test = train_test_split(
         X, y, test_size=test_size, stratify=y, random_state=random_seed
     )
 
+    # Split the validation data in validation and test data
     val_frac = val_size / (1 - test_size)
     X_train, X_val, y_train, y_val = train_test_split(
         X_trainval, y_trainval, test_size=val_frac, stratify=y_trainval, random_state=random_seed
     )
 
+    # Scale single datasets
     scaler = StandardScaler()
     X_train = scaler.fit_transform(X_train)
     X_val = scaler.transform(X_val)
     X_test = scaler.transform(X_test)
 
     def make_loader(X_arr, y_arr, shuffle=True):
+        """
+        Create a dataloader out of dataset
+        :return: torch.Dataloader
+        """
         dataset = TensorDataset(torch.tensor(X_arr), torch.tensor(y_arr))
         return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
 
@@ -72,7 +69,7 @@ def create_loaders(X, y, batch_size, test_size=0.2, val_size=0.1, random_seed=42
         make_loader(X_test, y_test, shuffle=False),
     )
 
-# Count total number of runs
+# Count total number of runs just for visualization
 total_runs = len(config["imbalance_ratios"]) * len(config["num_layers"]) * \
              sum(len(config["focal_alpha"]) * len(config["focal_gamma"]) if l == "focal"
                  else len(config["ce_weights"]) if l == "wce"
@@ -81,19 +78,19 @@ total_runs = len(config["imbalance_ratios"]) * len(config["num_layers"]) * \
 
 current_run = 0
 
-# --------------------------
 # Training loop over imbalance ratios and model configs
-# --------------------------
 for ratio in config["imbalance_ratios"]:
-    df_min = df[df["Class"] == 1]
-    df_maj = df[df["Class"] == 0]
-    keep_n = int(len(df_maj) * ratio)
-    df_maj_sampled = df_maj.sample(keep_n, random_state=config["random_seed"])
-    df_sub = pd.concat([df_min, df_maj_sampled], axis=0).sample(frac=1, random_state=config["random_seed"])
+    df_maj = df[df["Class"] == 0] # Majority class
+    df_min = df[df["Class"] == 1] # Minority class
+    keep_n = int(len(df_maj) * ratio) # Number of samples we want to keep to maintain the wanted imbalance ratio
+    df_maj_sampled = df_maj.sample(keep_n, random_state=config["random_seed"]) # Samples we want to keep based on the imbalance ratio
+    df_sub = pd.concat([df_min, df_maj_sampled], axis=0).sample(frac=1, random_state=config["random_seed"]) # Subset concatenated with the new samples
 
+    # Split into Class and Samples
     X = df_sub.drop(columns=["Class"]).values.astype(np.float32)
     y = df_sub["Class"].values.astype(np.float32)
 
+    # Create loaders from our new sampled dataset
     train_loader, val_loader, test_loader = create_loaders(
         X, y, batch_size=config["batch_size"], test_size=config["test_size"], val_size=config["val_size"], random_seed=config["random_seed"]
     )
@@ -103,8 +100,9 @@ for ratio in config["imbalance_ratios"]:
     num_majority = len(df_maj_sampled)
     imbalance_ratio = num_majority / num_minority
 
-    experiment_start_time = time.time()
+    # # If we have multiple models iterate through them and all our loss functions
     for num_layer, loss_fn in itertools.product(config["num_layers"], config["losses"]):
+        # Get our parameter configurations per loss
         if loss_fn == "focal":
             param_combinations = itertools.product(config["focal_alpha"], config["focal_gamma"])
         elif loss_fn == "wce":
@@ -112,7 +110,9 @@ for ratio in config["imbalance_ratios"]:
         else:
             param_combinations = [(None,)]
 
+        # Go through all the parameter combinations
         for params in param_combinations:
+            # Prints for starting the run with metrics
             current_run += 1
             print("="*70)
             print(f"RUN {current_run}/{total_runs} STARTED")
@@ -140,9 +140,8 @@ for ratio in config["imbalance_ratios"]:
             r2=f"{(num_majority / num_minority):.2f}"
             run_name = f"r{ratio}={r2}_{loss_fn}_{loss_param_str}"
 
-            run_start_time = time.time()
+            # Start new run with mlflow tracking
             with mlflow.start_run(run_name=run_name):
-                # Log params
                 mlflow.log_param("imbalance_ratio", imbalance_ratio)
                 mlflow.log_param("subsample_ratio", ratio)
                 mlflow.log_param("num_majority", num_majority)
@@ -155,7 +154,7 @@ for ratio in config["imbalance_ratios"]:
                 mlflow.log_param("learning_rate", config["lr"])
                 mlflow.log_param("epochs", config["epochs"])
 
-                # Initialize model and loss as before
+                # Initialize model and loss based on our current selected loss and hyperparameters
                 model = MLP(input_dim=input_dim, hidden_size=config["hidden_size"], num_layers=num_layer)
                 optimizer = torch.optim.Adam(model.parameters(), lr=config["lr"])
                 if loss_fn == "ce":
@@ -188,7 +187,7 @@ for ratio in config["imbalance_ratios"]:
                         total_loss += loss.item()
                     epoch_loss = total_loss / len(train_loader)
 
-                    # Validation
+                    # Validation during training
                     model.eval()
                     val_loss = 0.0
                     all_labels, all_preds = [], []
@@ -204,12 +203,15 @@ for ratio in config["imbalance_ratios"]:
                     val_loss /= len(val_loader)
                     val_f1 = f1_score(all_labels, all_preds)
 
+                    # Track loss for training and validation, as well as f1 score
                     mlflow.log_metric("train_loss", epoch_loss, step=epoch)
                     mlflow.log_metric("val_loss", val_loss, step=epoch)
                     mlflow.log_metric("val_f1", val_f1, step=epoch)
 
                     if (epoch+1)%10==0:
                         print(f"Train Loss: {epoch_loss:.4f}, Val Loss: {val_loss:.4f}, Val F1: {val_f1:.4f}")
+
+                    # Track the best F1 score so far
                     if val_f1 > best_val_f1:
                         best_val_f1 = val_f1
                         best_model_state = copy.deepcopy(model.state_dict())
@@ -227,23 +229,24 @@ for ratio in config["imbalance_ratios"]:
                         all_labels.extend(batch_y.numpy())
                         all_preds.extend(preds.numpy())
 
+                # Test eval scores
                 accuracy = accuracy_score(all_labels, all_preds)
                 precision = precision_score(all_labels, all_preds)
                 recall = recall_score(all_labels, all_preds)
                 f1 = f1_score(all_labels, all_preds)
                 cm = confusion_matrix(all_labels, all_preds)
 
+                # Track metrics for training
                 mlflow.log_metric("accuracy", accuracy)
                 mlflow.log_metric("precision", precision)
                 mlflow.log_metric("recall", recall)
                 mlflow.log_metric("f1_score", f1)
 
-                run_end_time = time.time() - run_start_time
+                # Prints for finishing the current run with evals
                 print("-"*90)
-                print(f"RUN {current_run}/{total_runs} FINISHED in {run_end_time:.2f} seconds")
+                print(f"RUN {current_run}/{total_runs} FINISHED")
                 print(f"Final Test Metrics -> Accuracy: {accuracy:.4f}, Precision: {precision:.4f}, Recall: {recall:.4f}, F1: {f1:.4f}")
                 print("-"*90)
 
                 mlflow.pytorch.log_model(model, name="MLP", input_example=X[:1])
-    experiment_end_time = time.time() - experiment_start_time
-    print(f"Experiment finished in {run_end_time:.2f} seconds")
+    print(f"Experiment finished.")
--- README.md
+++ README.md
@@ -3,31 +3,20 @@
 Implement and analyze Focal Loss for handling class imbalance. Compare its convergence and final F1-score performance against a standard weighted cross-entropy.
 
 ## Roadmap
-- 
-
-## To-Do List
-
-- [ ] Implement Focal Loss - **Jannis**
-- [ ] Implement Weighted Cross-Entropy (Make use of PyTorch's WCE)- **Yannick**
-- [ ] Define Dataset - **tbd.**
-- [ ] Setup first training pipline - **tbd.**
-- [ ] Implement framework for experiment tracking - **tbd.**
+1. Research about weighted cross-entropy, focal loss, behaviour and $\alpha$-balanced versions
+2. Implement the vocal loss
+3. Test on artificial dataset (make moons)
+4. Evaluate artificial dataset results
+5. Create real world example (credit card fraud)
+6. 
 
 ## Definitions
 ### Focal Loss
 
 Focal Loss is a modified cross-entropy loss that addresses class-imbalance by down-weighting easy, well-classified examples and focusing training on hard ones. It is defined as:
-
 $$
 \text{FL}(p_t) = -\alpha_t \,(1 - p_t)^{\gamma}\;\log(p_t)
 $$
-> Comment: <br>
-> Questions: <br>
-> Do we really need the $\alpha$ there? Why? <br>
-> Isn't the Focal loss a weighted cross entropy? <br>
-> Why does it do what focal loss does? <br>
-> How and why is that better than WCE? Why does WCE and CE still exist then? pros/cons <br>
-> 
 where $\alpha_t$ balances classes and $\gamma$ controls the focus on hard samples.  
 Introduced in [Lin et al., 2017 — *Focal Loss for Dense Object Detection*](https://arxiv.org/pdf/1708.02002).
 
@@ -40,38 +29,3 @@ $$
 $$
 \text{CE}(p_t) = -\log(p_t)
 $$
-
-# Presentation
-1. Task
-2. Agenda
-1. Motivation
-   2. Imbalanced Datasets (Balancing methods for datasets)
-   4. Leaving the dataset unbalanced (examples)
-   5. Dealing with Imbalanced datasets during training
-2. Introduction & Related Work
-   3. Loss Types
-      3. Normal Loss
-      4. Cross Entropy (Log Loss)
-      5. Weighted Cross Entropy 
-         6. (https://link.springer.com/article/10.1007/s11063-018-09977-1 can't access)
-         7. https://arxiv.org/pdf/2006.01413
-         8. https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=10502684
-      6. Focal Loss 
-         7. (Cite Paper https://arxiv.org/pdf/1708.02002)
-         7. https://proceedings.neurips.cc/paper_files/paper/2020/file/aeb7b30ef1d024a76f21a1d40e30c302-Paper.pdf
-   7. Compare loss functions mathematically and visually
-   8. Explain those loss functions
-3. Experiment
-   4. Dataset
-   5. Setup
-   6. Model
-   7. Method (MLFlow) & Training method (How we ran the trainings)
-   8. Results (visually: make it pretty with graphs)
-4. Conclusion
-   5. What do the graphs tell us?
-   6. Experiments findings
-   7. What do those findings mean?
-   8. How are they related to the real world examples from the beginning?
-   9. Which loss function is better now? When?
-   10. What is our message to the world?
-12. Sources
